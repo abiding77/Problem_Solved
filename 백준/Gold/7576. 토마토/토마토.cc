a0ea66771bdc4e1981582130e7f26397394ff259
Jshrewd <50572717+abiding77@users.noreply.github.com>
2023-03-20 19:38:55 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int M, N;
+int board[1002][1002];
+int dist[1002][1002];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> M >> N;
+	queue<pair<int, int>> Q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 1)
+				Q.push({ i,j });
+			if (board[i][j] == 0)
+				dist[i][j] = -1;
+		}
+	}
+	while (!Q.empty()) {
+		pair<int, int> cur = Q.front();
+		Q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur.first + dx[dir];
+			int ny = cur.second + dy[dir];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+			if (dist[nx][ny] >= 0) continue;
+			dist[nx][ny] = dist[cur.first][cur.second] + 1;
+			Q.push({ nx,ny });
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (dist[i][j] == -1) {
+				cout << -1;
+				return 0;
+			}
+			ans = max(ans, dist[i][j]);
+		}
+	}
+	cout << ans;
+}
