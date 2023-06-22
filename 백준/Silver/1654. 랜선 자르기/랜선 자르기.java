71688d54e8c0074ae6531c9086be1af141e772a3
Rendy <50572717+abiding77@users.noreply.github.com>
2023-06-22 15:12:06 +0900

    [Silver II] Title: 랜선 자르기, Time: 184 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int arr[] = new int[K];
+
+        long max = 0;
+        for(int i=0; i<K; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            if(max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        max++;
+        long min = 0;
+        long mid = 0;
+        while(min < max){
+            mid = (min + max) / 2;
+            long cnt = 0;
+            for(int i=0; i<arr.length; i++){
+                cnt += (arr[i] / mid);
+            } 
+            if(cnt < N){
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min-1);
+
+    }
+}
