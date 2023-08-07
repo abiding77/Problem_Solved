07ac8f5823b3b568b4b9c2a2e3b833d1b4c1efa3
Rendy <50572717+abiding77@users.noreply.github.com>
2023-08-07 18:09:14 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 40 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,27 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
+
 using namespace std;
 
+int R, C, T;
+int totalDust;
 int dr[4] = { 0,1,0,-1 };
 int dc[4] = { 1,0,-1,0 };
+int up_row, down_row;
 int board[51][51];
 int addArr[51][51];
-int R, C, T;
-int up_row, down_row;
-int totalDust;
-/*
-1. 미세먼지가 확산된다. 확산은 미세먼지가 있는 모든 칸에서 동시에 일어난다.
-(r, c)에 있는 미세먼지는 인접한 네 방향으로 확산된다.
-인접한 방향에 공기청정기가 있거나, 칸이 없으면 그 방향으로는 확산이 일어나지 않는다.
-확산되는 양은 Ar,c/5이고 소수점은 버린다.
-(r, c)에 남은 미세먼지의 양은 Ar,c - (Ar,c/5)×(확산된 방향의 개수) 이다.
-2. 공기청정기가 작동한다.
-공기청정기에서는 바람이 나온다.
-위쪽 공기청정기의 바람은 반시계방향으로 순환하고, 아래쪽 공기청정기의 바람은 시계방향으로 순환한다.
-바람이 불면 미세먼지가 바람의 방향대로 모두 한 칸씩 이동한다.
-공기청정기에서 부는 바람은 미세먼지가 없는 바람이고, 공기청정기로 들어간 미세먼지는 모두 정화된다.
-*/
 void input() {
 	bool flag = false;
 	for (int i = 0; i < R; i++) {
@@ -32,34 +19,32 @@ void input() {
 					up_row = i;
 					flag = true;
 				}
-				else {
+				else
 					down_row = i;
-				}
 			}
-			else {
+			else
 				totalDust += board[i][j];
-			}
 		}
 	}
 }
+
 void spread() {
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
-			int cnt = 0;
 			int val = board[i][j] / 5;
+			int cnt = 0;
 			if (board[i][j] == 0 || board[i][j] == -1) continue;
 			for (int k = 0; k < 4; k++) {
 				int nr = i + dr[k];
 				int nc = j + dc[k];
-				if (nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
 				if (board[nr][nc] == -1) continue;
+				if (nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
 				addArr[nr][nc] += val;
 				cnt++;
 			}
-			addArr[i][j] -= (cnt * val);
+			addArr[i][j] -= (val * cnt);
 		}
 	}
-
 	for (int i = 0; i < R; i++) {
 		for (int j = 0; j < C; j++) {
 			board[i][j] += addArr[i][j];
@@ -72,37 +57,38 @@ void airCleaner() {
 	totalDust -= board[up_row - 1][0];
 	totalDust -= board[down_row + 1][0];
 
-	// 위 공기 순환 ( 반시계)
+	// 위쪽 순회
 	// 1. 왼쪽줄
-	for (int i = up_row - 1; i > 0; i--)
-		board[i][0] = board[i - 1][0];
-	// 2. 윗줄
+	for (int i = up_row - 1; i > 0; i--) 
+		board[i][0] = board[i-1][0];
+	// 2. 윗쪽줄
 	for (int i = 0; i < C - 1; i++)
-		board[0][i] = board[0][i + 1];
-	// 3. 오른쪽줄
+		board[0][i] = board[0][i+1];
+	// 3. 오른쪽줄 // [0][0] = [1][0] , [1][0] = [2][0]
 	for (int i = 1; i <= up_row; i++)
-		board[i - 1][C - 1] = board[i][C - 1];
-	// 4. 아랫줄
+		board[i-1][C-1] = board[i][C-1];
+	// 4. 아래줄 // 7 = 6 6 = 5 5 = 4 4 = 3 3 = 2 2 = 1
 	for (int i = C - 1; i > 1; i--)
 		board[up_row][i] = board[up_row][i - 1];
 	board[up_row][1] = 0;
 
-	// 아래 공기 순환 (시계)
-	// 1. 왼쪽줄 [4][0] = [5][0]   , [5][0] = [6][0];
+	// 아래쪽 순회
+	// 1. 왼쪽줄
 	for (int i = down_row + 1; i < R - 1; i++)
-		board[i][0] = board[i + 1][0];
-	// 2. 아랫줄
+		board[i][0] = board[i+1][0];
+	// 2. 아래쪽줄
 	for (int i = 0; i < C - 1; i++)
-		board[R - 1][i] = board[R - 1][i + 1];
+		board[R - 1][i] = board[R - 1][i+1];
 	// 3. 오른쪽줄
-	for (int i = R - 1; i >= down_row; i--)
+	for (int i = R - 1; i >= down_row+1; i--)  // 6 = 5 ,5 = 4 , 4=3
 		board[i][C - 1] = board[i - 1][C - 1];
-	// 4. 윗줄
-	for (int i = C - 1; i > 1; i--)
+	
+	// 4. 윗족줄
+	for (int i = C - 1; i >= 2; i--) 
 		board[down_row][i] = board[down_row][i - 1];
 	board[down_row][1] = 0;
-
 }
+
 void solve() {
 	while (T--) {
 		spread();
@@ -110,15 +96,6 @@ void solve() {
 	}
 	cout << totalDust << '\n';
 }
-//void print() {
-//	for (int i = 0; i < R; i++) {
-//		for (int j = 0; j < C; j++) {
-//			cout << board[i][j] << " ";
-//		}
-//		cout << '\n';
-//	}
-//}
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -126,6 +103,5 @@ int main() {
 	cin >> R >> C >> T;
 	input();
 	solve();
-
 	return 0;
 }
