0827f18b3599828ad25970e76e4b62db6a24a5ec
Rendy <50572717+abiding77@users.noreply.github.com>
2023-08-14 18:13:05 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,24 +1,55 @@
+// --> next_permutation Version
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//int N, M;
+//int arr[10];
+//int pre[10];
+//int main() {
+//	cin >> N >> M;
+//	for (int i = 0; i < N; i++) {
+//		arr[i] = i + 1;
+//	}
+//	sort(arr, arr + N);
+//	do {
+//		bool flag = false;
+//		for (int i = 0; i < M; i++) {
+//			if (pre[i] != arr[i]) {
+//				flag = true;
+//				break;
+//			}
+//		}
+//		if (!flag) continue;
+//		for (int i = 0; i < M; i++) {
+//			cout << arr[i] << " ";
+//			pre[i] = arr[i];
+//		}
+//		cout << '\n';
+//	} while (next_permutation(arr, arr + N));
+//}
+
+// BackTraking Version
 #include <iostream>
 
 using namespace std;
-
-int n, m;
+int N, M;
 int arr[10];
-int isused[10];
-void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
+bool visited[10];
+void DFS(int num) {
+	if (num == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
 		}
 		cout << '\n';
 		return;
 	}
-	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) {
-			arr[k] = i;
-			isused[i] = 1;
-			func(k + 1);
-			isused[i] = 0;
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			arr[num] = i;
+			visited[i] = true;
+			DFS(num + 1);
+			visited[i] = false;
 		}
 	}
 }
@@ -26,6 +57,6 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	cin >> n >> m;
-	func(0);
+	cin >> N >> M;
+	DFS(0);
 }
