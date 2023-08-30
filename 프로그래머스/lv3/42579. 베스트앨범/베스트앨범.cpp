60956d986dacad595e32fffe8200fbc8140f6780
Rendy <50572717+abiding77@users.noreply.github.com>
2023-08-30 18:37:44 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
@@ -6,44 +6,43 @@
 
 using namespace std;
 
-bool cmp(pair<string, int> a, pair<string, int> b){
+bool cmp(pair<string,int> a, pair<string,int> b){
     return a.second > b.second;
 }
 
+bool cmp2(pair<int,int> a , pair<int,int> b){
+    if(a.second == b.second){
+        return a.first < b.first;
+    }
+    return a.second > b.second;
+}
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    map<string, int> m;
-    for(int i = 0; i < genres.size(); i++){
+    map<string,int> m;
+    for(int i=0; i<genres.size(); i++){
         m[genres[i]] += plays[i];
     }
-
-    vector<pair<string, int>> genrePlays;
+    vector<pair<string,int>> genresPlay;
     for(auto it : m){
-        genrePlays.push_back(make_pair(it.first, it.second));
+        genresPlay.push_back({it.first,it.second});
     }
-    sort(genrePlays.begin(), genrePlays.end(), cmp);
-
-    for(auto genrePlay : genrePlays){
-        string genre = genrePlay.first;
-        vector<pair<int, int>> songInfo;
-
-        for(int i = 0; i < genres.size(); i++){
-            if(genres[i] == genre){
-                songInfo.push_back(make_pair(i, plays[i]));
+    sort(genresPlay.begin(), genresPlay.end(),cmp);
+    for(auto genrePlay : genresPlay){
+        string songName = genrePlay.first;
+        vector<pair<int,int>> songInfo; // 인덱스 , 재생횟수
+        for(int i=0; i<genres.size(); i++){
+            if(songName == genres[i]){
+                songInfo.push_back({i,plays[i]});
             }
         }
-
-        sort(songInfo.begin(), songInfo.end(), [](pair<int, int> a, pair<int, int> b){
-            if(a.second == b.second){
-                return a.first < b.first;
-            }
-            return a.second > b.second;
-        });
-
-        for(int i = 0; i < songInfo.size() && i < 2; i++){
+        sort(songInfo.begin(),songInfo.end(),cmp2);
+        
+        for(auto it : songInfo){
+            cout << it.first << " " << it.second << '\n';
+        }
+        for(int i=0; i<songInfo.size() && i < 2 ; i++){
             answer.push_back(songInfo[i].first);
         }
     }
-
     return answer;
 }
