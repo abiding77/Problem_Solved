5e5f7449f3c58a7174a6bbccbd6581c27eb2e5f9
Rendy <50572717+abiding77@users.noreply.github.com>
2023-09-08 18:01:17 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	if (N % 2 == 0) cout << "CY";
+	else cout << "SK";
+}
